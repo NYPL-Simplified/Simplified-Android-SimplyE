23e3ce9b87ea0a6f49908e0aba0dc47dc64cf37e
malcolmmcfly <mjw0133@gmail.com>
2020-04-30 08:42:47 -0500

    Updated core version to 5.2.0

--- app/src/main/java/org/nypl/simplified/simplye/SimplyEAccountFallback.kt
+++ app/src/main/java/org/nypl/simplified/simplye/SimplyEAccountFallback.kt
@@ -3,7 +3,7 @@ package org.nypl.simplified.simplye
 import org.joda.time.DateTime
 import org.nypl.simplified.accounts.api.AccountProviderAuthenticationDescription
 import org.nypl.simplified.accounts.api.AccountProviderFallbackType
-import org.nypl.simplified.accounts.api.AccountProviderImmutable
+import org.nypl.simplified.accounts.api.AccountProvider
 import org.nypl.simplified.accounts.api.AccountProviderType
 import java.net.URI
 
@@ -14,18 +14,19 @@ import java.net.URI
 class SimplyEAccountFallback : AccountProviderFallbackType {
 
   override fun get(): AccountProviderType {
-    return AccountProviderImmutable(
+    return AccountProvider(
       addAutomatically = false,
       annotationsURI = null,
       authenticationDocumentURI = URI.create("https://circulation.librarysimplified.org/CLASSICS/authentication_document"),
       authentication = AccountProviderAuthenticationDescription.COPPAAgeGate(
-        greaterEqual13 = URI.create("https://circulation.librarysimplified.org/CLASSICS/groups/1831"),
-        under13 = URI.create("https://circulation.librarysimplified.org/CLASSICS/groups/1832")),
+      greaterEqual13 = URI.create("https://circulation.librarysimplified.org/CLASSICS/groups/1831"),
+      under13 = URI.create("https://circulation.librarysimplified.org/CLASSICS/groups/1832")),
       cardCreatorURI = null,
       catalogURI = URI.create("https://circulation.librarysimplified.org/CLASSICS/groups/1832"),
       displayName = "The SimplyE Collection",
       eula = null,
       id = URI.create("urn:uuid:56906f26-2c9a-4ae9-bd02-552557720b99"),
+      idNumeric = -1,
       isProduction = true,
       license = URI.create("http://www.librarysimplified.org/iclicenses.html"),
       loansURI = URI.create("https://circulation.librarysimplified.org/CLASSICS/loans/"),

300ae937a48aa9831daed790db03c2692eaad8a5
Mark Raynsford <code@io7m.com>
2020-11-16 18:49:29 +0000

    Enable GitHub Actions
    This enables GitHub Actions and updates Gradle and plugins to the
    latest versions.
    
    Affects: https://jira.nypl.org/browse/SIMPLY-2882

--- .travis-git-props.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-GP_GIT_COMMIT=$(git rev-list --max-count=1 HEAD) || exit 1
-SIMPLYE_VERSION=$(grep versionCode app/version.properties | sed 's/versionCode/simplye.versionCode/g') || exit 1
-
-cat <<EOF
-git.commit=${GP_GIT_COMMIT}
-${SIMPLYE_VERSION}
-EOF
